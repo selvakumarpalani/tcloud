bc0b5e70c1270756692c58e9e414546d7adb037b
selvakumarpalani <57978145+selvakumarpalani@users.noreply.github.com>
2020-07-29 21:40:46 +1000

    Update datasource.tf

--- datasource.tf
+++ datasource.tf
@@ -2,16 +2,5 @@
 
 resource "aws_instance" "myec2" {
    ami = "ami-0a58e22c727337c51"
-   instance_type = lookup(var.instance_type,terraform.workspace)
-   region = "ap-southeast-2"
 }
 
-variable "instance_type" {
-  type = "map"
-
-  default = {
-    default = "t2.nano"
-    dev     = "t2.micro"
-    prd     = "t2.large"
-  }
-}
